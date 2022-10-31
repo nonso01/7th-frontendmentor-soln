e0d3b31119cba7b480abd14a1b2fed4b9b360f0c
nonso01 <chukwunonsomartin01@gmail.com>
2022-10-31 23:09:54 +0100

    setting up

---
+++ src/fun.js
@@ -0,0 +1,25 @@
+const csl=(x)=>console.log(x)
+const dc=(x)=>document.createElement(x)
+const dq=(x)=>document.querySelector(x)
+const di=(x)=> document.getElementById(x)
+const dqA=(x)=> document.querySelectorAll(x)
+
+const html=dq("html")
+const body= dq('body')
+
+
+function interval(f,time){
+const interval= setInterval(f,time)
+}
+
+function timeOut(f,time){
+ const timeOut= setTimeout(f,time)
+}
+
+function event(e,t,f){
+ const event= e.addEventListener(t,f)
+}
+
+function rmEvent(e,t,f){
+ const remove= e.removeEventListener(t,f)
+}
